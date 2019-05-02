adbfc251133fcf2d34c03306e710063ad371c5ae
drproduck <duckhiem95@yahoo.com>
2019-05-01 20:07:24 -0700

    added image segmentation

--- snake.py
+++ snake.py
@@ -1,106 +1,85 @@
+import random
 import sys,os
 import curses
 
-class snake:
+UP = ord('w')
+DOWN = ord('s')
+LEFT = ord('a')
+RIGHT = ord('d')
+QUIT = ord('q')
+block = '|||'
+
+def get_next_move(snake, key):
+    if key == UP:
+        head = (snake[-1][0] - 1, snake[-1][1])
+    elif key == DOWN:
+        head = (snake[-1][0] + 1, snake[-1][1])
+    elif key == LEFT:
+        head = (snake[-1][0], snake[-1][1] - 1)
+    elif key == RIGHT:
+        head = (snake[-1][0], snake[-1][1] + 1)
+    else: head = None
+    return head
   
 
-def draw_menu(stdscr):
-    k = 0
-    cursor_x = 0
-    cursor_y = 0
-
+def draw_snake(stdscr):
     # Clear and refresh the screen for a blank canvas
     stdscr.clear()
     stdscr.refresh()
 
-    # Start colors in curses
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-
-    UP = ord('w')
-    DOWN = ord('s')
-    LEFT = ord('a')
-    RIGHT = ord('d')
-    block = '|||'
-
-    # Loop where k is the last character pressed
-    k = LEFT
-    # Initialization
-    stdscr.clear()
     height, width = stdscr.getmaxyx()
 
-    y_init = height / 2
-    x_init = width / 4
+    y_init = height // 2
+    x_init = width // 4
 
-    while (k != ord('q')):
+    snake = [(y_init, x_init)]
+
+    food = (int(height * random.random()), int(width * random.random()) )
+
+    oldkey = LEFT
+    key = oldkey
+
+    while (key != QUIT):
+        
+        stdscr.clear()
+
+        head = get_next_move(snake, key)
+
+        # check if invalid input or it goes to itself
+        if head is None and (len(snake) > 1 and head == snake[-2]):
+            key = oldkey
+            head = get_next_move(snake, key)
 
 
-        if k == curses.KEY_DOWN:
-            cursor_y = cursor_y + 1
-        elif k == curses.KEY_UP:
-            cursor_y = cursor_y - 1
-        elif k == curses.KEY_RIGHT:
-            cursor_x = cursor_x + 1
-        elif k == curses.KEY_LEFT:
-            cursor_x = cursor_x - 1
+        # check if it goes out of bound
+        if head[0] < 0 or head[0] > height - 1 or head[1] < 0 or head[1] > width - 1:
+                break
 
-        cursor_x = max(0, cursor_x)
-        cursor_x = min(width-1, cursor_x)
+        # else move forward
+        oldkey = key
+        if head == food:
+            snake.append(head)
+            food = (int(height * random.random()), int(width * random.random()) )
 
-        cursor_y = max(0, cursor_y)
-        cursor_y = min(height-1, cursor_y)
+        else:
+            snake.append(head)
+            snake.pop(0)
+            
+        
+        for b in snake:
+            stdscr.addch(b[0], b[1], curses.ACS_PI)
 
-        # Declaration of strings
-        title = "Curses example"[:width-1]
-        subtitle = "Written by Clay McLeod"[:width-1]
-        keystr = "Last key pressed: {}".format(k)[:width-1]
-        statusbarstr = "Press 'q' to exit | STATUS BAR | Pos: {}, {}".format(cursor_x, cursor_y)
-        if k == 0:
-            keystr = "No key press detected..."[:width-1]
+        stdscr.addstr(food[0], food[1], block)
 
-        # Centering calculations
-        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
-        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
-        start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
-        start_y = int((height // 2) - 2)
-
-        # Rendering some text
-        whstr = "Width: {}, Height: {}".format(width, height)
-        stdscr.addstr(0, 0, whstr, curses.color_pair(1))
-
-        # Render status bar
-        stdscr.attron(curses.color_pair(3))
-        stdscr.addstr(height-1, 0, statusbarstr)
-        stdscr.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-        stdscr.attroff(curses.color_pair(3))
-
-        # Turning on attributes for title
-        stdscr.attron(curses.color_pair(2))
-        stdscr.attron(curses.A_BOLD)
-
-        # Rendering title
-        stdscr.addstr(start_y, start_x_title, title)
-
-        # Turning off attributes for title
-        stdscr.attroff(curses.color_pair(2))
-        stdscr.attroff(curses.A_BOLD)
-
-        # Print rest of text
-        stdscr.addstr(start_y + 1, start_x_subtitle, subtitle)
-        stdscr.addstr(start_y + 3, (width // 2) - 2, '-' * 4)
-        stdscr.addstr(start_y + 5, start_x_keystr, keystr)
-        stdscr.move(cursor_y, cursor_x)
 
         # Refresh the screen
         stdscr.refresh()
 
         # Wait for next input
-        k = stdscr.getch()
+        key = stdscr.getch()
 
 def main():
-    curses.wrapper(draw_menu)
+    curses.wrapper(draw_snake)
 
 if __name__ == "__main__":
     main()
